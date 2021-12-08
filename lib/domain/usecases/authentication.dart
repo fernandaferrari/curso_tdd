93acfe5b5089e7bc2dfef062581ee8f620587274
Fernanda <fernanda@inovadora.com.br>
2021-12-08 17:43:13 -0300

    criado Authentication params.

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,8 +1,12 @@
-import 'package:meta/meta.dart';
-
 import '../entities/account_entity.dart';
 
 abstract class IAuthentication {
-  Future<AccountEntity> auth(
-      {@required String email, @required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({required this.email, required this.secret});
 }
