9d60aead29613d9278526202cb97fa2f9d42a40b
Fernanda <fernanda@inovadora.com.br>
2021-12-08 17:25:12 -0300

    implementação do teste remote_authentication.

---
+++ test/data/usercases/remote_authentication_test.dart
@@ -0,0 +1,39 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'remote_authentication_test.mocks.dart';
+
+class RemoteAuthentication {
+  final IHttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({required this.httpClient, required this.url});
+
+  Future<void> auth() async {
+    await httpClient.request(url: url);
+  }
+}
+
+abstract class IHttpClient {
+  Future<void> request({required String url});
+}
+
+//class HttpClientMock extends Mock implements IHttpClient {}
+
+@GenerateMocks([IHttpClient])
+void main() {
+  test('Quando usar a URL certa HTTPClient', () async {
+    final httpClient = MockIHttpClient();
+
+    final url = faker.internet.httpUrl();
+
+    //classe que sempre esta testando = sut
+    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+
+    await sut.auth();
+
+    verify(httpClient.request(url: url));
+  });
+}
