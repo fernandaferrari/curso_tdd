297a73ddf75a7ed4d7fa29140f27b13279fddf0f
Fernanda <fernanda@inovadora.com.br>
2021-12-08 16:24:45 -0300

    add autenticaçao usecase

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta.dart';
+
+import '../entities/account_entity.dart';
+
+abstract class IAuthentication {
+  Future<AccountEntity> auth(
+      {@required String email, @required String password});
+}
