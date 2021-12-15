e3fec14236775c18334b74b7a0c853bdb9768c7c
Fernanda <fernanda@inovadora.com.br>
2021-12-15 11:37:40 -0300

    refatoracao camada presentation e validaçoes de erro streampresenter.

---
+++ lib/presentation/presenter/dependencies/validation.dart
@@ -0,0 +1,3 @@
+abstract class IValidation {
+  String? validate({required String field, required String value});
+}
---
+++ lib/presentation/presenter/stream_login_presenter.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:curso_tdd/presentation/presenter/dependencies/dependencies.dart';
+
+class LoginState {
+  String? emailError;
+
+  bool get isFormValid => false;
+}
+
+class StreamLoginPresenter {
+  final IValidation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  final _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError!).distinct();
+  Stream<bool> get isFormValidStream =>
+      _controller.stream.map((state) => state.isFormValid).distinct();
+
+  StreamLoginPresenter({required this.validation});
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
