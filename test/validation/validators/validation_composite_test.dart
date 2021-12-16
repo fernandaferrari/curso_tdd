55629204e8d940da8d028da9314051a1d470cc73
Fernanda <fernanda@inovadora.com.br>
2021-12-16 14:13:31 -0300

    teste para que retorne o primeiro erro encontrado dentre as validações do field correto

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 
 import 'package:curso_tdd/presentation/presenter/dependencies/validation.dart';
@@ -12,7 +13,14 @@ class ValidationComposite implements IValidation {
 
   @override
   String? validate({required String? field, required String? value}) {
-    return null;
+    String? error;
+    for (final validation in validations.where((v) => v.field == field)) {
+      error = validation.validate(value!);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error!.isEmpty ? null : error;
   }
 }
 
@@ -20,20 +28,39 @@ class FieldValidationMock extends Mock implements FieldValidation {}
 
 void main() {
   late ValidationComposite sut;
-  final validator1 = FieldValidationMock();
-  final validator2 = FieldValidationMock();
+  late FieldValidationMock validator1;
+  late FieldValidationMock validator2;
+
+  void mockValidator1(String? error) {
+    when(() => validator1.validate('any_value')).thenReturn(error);
+  }
+
+  void mockValidator2(String? error) {
+    when(() => validator2.validate('any_value')).thenReturn(error);
+  }
 
   setUp(() {
+    validator1 = FieldValidationMock();
+    validator2 = FieldValidationMock();
+    when(() => validator1.field).thenReturn('other_field');
+    mockValidator1(null);
+    when(() => validator2.field).thenReturn('any_field');
+    mockValidator2(null);
+
     sut = ValidationComposite([validator1, validator2]);
   });
   test('Should return null if all validations return null or empty', () {
-    when(() => validator1.field).thenReturn('any_field');
-    when(() => validator1.validate('any_value')).thenReturn(null);
-    when(() => validator2.field).thenReturn('any_field');
-    when(() => validator1.validate('any_value')).thenReturn('');
+    mockValidator2('');
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+    expect(error, null);
+  });
+
+  test('teste para que retorne o primeiro erro encontrado dentre as validações',
+      () {
+    mockValidator1('error_1');
+    mockValidator2('error_2');
 
     final error = sut.validate(field: 'any_field', value: 'any_value');
-
-    expect(error, null);
+    expect(error, 'error_2');
   });
 }
