363543a2eafa8f92bab76c71a78a7af82f8c6679
Fernanda <fernanda@inovadora.com.br>
2021-12-20 16:03:04 -0300

    add getx login presenter

--- lib/main/factories/pages/login/login_pages_factory.dart
+++ lib/main/factories/pages/login/login_pages_factory.dart
@@ -3,5 +3,5 @@ import 'package:curso_tdd/ui/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 Widget makeLoginPage() {
-  return LoginPage(makeLoginPresenter());
+  return LoginPage(makeGetxLoginPresenter());
 }
--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -4,7 +4,14 @@ import 'package:curso_tdd/main/factories/usercases/usecases.dart';
 import 'package:curso_tdd/presentation/presenter/stream_login_presenter.dart';
 import 'package:curso_tdd/ui/pages/pages.dart';
 
-ILoginPresenter makeLoginPresenter() {
+ILoginPresenter makeStreamLoginPresenter() {
+  return StreamLoginPresenter(
+    authentication: makeRemoteAuthentication(),
+    validation: makeLoginValidation(),
+  );
+}
+
+ILoginPresenter makeGetxLoginPresenter() {
   return StreamLoginPresenter(
     authentication: makeRemoteAuthentication(),
     validation: makeLoginValidation(),
