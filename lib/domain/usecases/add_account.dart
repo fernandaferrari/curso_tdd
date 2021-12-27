ee720468aac39d3bb915c0a8141176a9093573d4
Fernanda <fernanda@inovadora.com.br>
2021-12-27 11:09:39 -0300

    add addAccount useCase

---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import '../entities/account_entity.dart';
+import 'package:meta/meta.dart';
+
+abstract class AddAccount {
+  Future<AccountEntity> add(AddAccountParams params);
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  const AddAccountParams({
+    @required this.email,
+    @required this.password,
+    @required this.name,
+    @required this.passwordConfirmation,
+  });
+
+  toJson() => {
+        "email": email,
+        "password": password,
+        "name": name,
+        "passwordConfirmation": passwordConfirmation
+      };
+
+  @override
+  List<Object> get props => [email, password, name, passwordConfirmation];
+}
