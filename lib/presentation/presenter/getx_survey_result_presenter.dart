aaf60009e64be3fde1db50866772a7f2357ac936
Fernanda <fernanda@inovadora.com.br>
2022-02-04 15:12:45 -0300

    remove duplication GetxSurveyResultPresenter

--- lib/presentation/presenter/getx_survey_result_presenter.dart
+++ lib/presentation/presenter/getx_survey_result_presenter.dart
@@ -1,3 +1,4 @@
+import 'package:curso_tdd/domain/entities/entities.dart';
 import 'package:curso_tdd/presentation/mixins/mixins.dart';
 import 'package:curso_tdd/ui/pages/pages.dart';
 import 'package:get/get.dart';
@@ -26,36 +27,19 @@ class GetxSurveyResultPresenter extends GetxController
       _surveysResult.stream;
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult =
-          await loadSurveyResultStream.loadBySurvey(surveyId: surveyId);
-      _surveysResult.value = SurveysResultViewModel(
-          question: surveyResult.question,
-          surveyId: surveyResult.surveyId,
-          answers: surveyResult.answers
-              .map((answer) => SurveyAnswerViewModel(
-                  image: answer.image,
-                  answer: answer.answer,
-                  isCurrentAnswer: answer.isCurrentAnswer,
-                  percent: '${answer.percent}'))
-              .toList());
-    } on DomainError catch (error) {
-      if (error == DomainError.acessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveysResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(
+        () => loadSurveyResultStream.loadBySurvey(surveyId: surveyId));
   }
 
   @override
   Future<void> save({@required String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
-      final surveyResult = await saveSurveyResult.save(answer: answer);
+      final surveyResult = await action();
       _surveysResult.value = SurveysResultViewModel(
           question: surveyResult.question,
           surveyId: surveyResult.surveyId,
