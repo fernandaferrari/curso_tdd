0ecfa8b48d43acd772972b8ba9534c2fd30831ba
Fernanda <fernanda@inovadora.com.br>
2022-02-04 14:07:27 -0300

    ensure RemoteSaveResult returns SurveyResult success

--- test/data/usercases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usercases/save_survey_result/remote_save_survey_result_test.dart
@@ -1,4 +1,7 @@
+import 'package:curso_tdd/data/model/model.dart';
+import 'package:curso_tdd/domain/entities/entities.dart';
 import 'package:curso_tdd/domain/helpers/helpers.dart';
+import 'package:curso_tdd/domain/usecases/save_survey_result.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -8,7 +11,7 @@ import 'package:curso_tdd/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements IHttpClient {}
 
-class RemoteSaveSurveyResult {
+class RemoteSaveSurveyResult implements SaveSurveyResult {
   final IHttpClient httpClient;
   final String url;
 
@@ -17,10 +20,11 @@ class RemoteSaveSurveyResult {
     @required this.url,
   });
 
-  Future<void> save({String answer}) async {
+  Future<SurveyResultEntity> save({String answer}) async {
     try {
-      await httpClient
+      final json = await httpClient
           .request(url: url, method: 'put', body: {'answer': answer});
+      return RemoteSurveyResultModel.fromJson(json).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.forbidden
           ? DomainError.acessDenied
@@ -35,11 +39,38 @@ void main() {
   String url;
   String answer;
 
+  Map surveyResult;
+
+  Map mockValidData() => {
+        'surveyId': faker.guid.guid(),
+        'question': faker.randomGenerator.string(50),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+          {
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          }
+        ],
+        'date': faker.date.dateTime().toIso8601String(),
+      };
+
   PostExpectation mockRequestCall() => when(httpClient.request(
       url: anyNamed("url"),
       method: anyNamed("method"),
       body: anyNamed("body"),
       headers: anyNamed("headers")));
+  void mockHttpData(dynamic data) {
+    surveyResult = data;
+    mockRequestCall().thenAnswer((_) async => data);
+  }
 
   void mockHttpError(HttpError error) => mockRequestCall().thenThrow(error);
 
@@ -48,6 +79,7 @@ void main() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
     sut = RemoteSaveSurveyResult(url: url, httpClient: httpClient);
+    mockHttpData(mockValidData());
   });
 
   test('Should call HttClient with correct values', () async {
@@ -55,6 +87,31 @@ void main() {
 
     verify(
         httpClient.request(url: url, method: 'put', body: {'answer': answer}));
+  });
+
+  test('Should return surveysResult on 200', () async {
+    final result = await sut.save(answer: answer);
+
+    expect(
+        result,
+        SurveyResultEntity(
+            surveyId: surveyResult['surveyId'],
+            question: surveyResult['question'],
+            answers: [
+              SurveyAnswerEntity(
+                image: surveyResult['answers'][0]['image'],
+                answer: surveyResult['answers'][0]['answer'],
+                isCurrentAnswer: surveyResult['answers'][0]
+                    ['isCurrentAccountAnswer'],
+                percent: surveyResult['answers'][0]['percent'],
+              ),
+              SurveyAnswerEntity(
+                answer: surveyResult['answers'][1]['answer'],
+                isCurrentAnswer: surveyResult['answers'][1]
+                    ['isCurrentAccountAnswer'],
+                percent: surveyResult['answers'][1]['percent'],
+              )
+            ]));
   });
 
   test('Quando ocorrer um erro inesperado pelo HttpClient returns 404',
